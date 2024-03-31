ff70c7ab5fdfd205ce2e324032bebc30e79c176d
matrosov <matrosov@100up.ru>
2024-04-01 02:46:46 +0400

    - projects settings, colors, shadows

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,26 +1,9 @@
 <script setup lang="ts">
-import Typography from '@/shared/typography/ui.vue'
+import Header from '@/widgets/Header/index.vue'
 </script>
 
 <template>
   <main>
-    <Typography tagName="p" size="s">Test</Typography>
-    <Typography tagName="p" size="xs">Test</Typography>
-    <Typography tagName="p" size="s">Test</Typography>
-    <Typography tagName="p" size="m">Test</Typography>
-    <Typography tagName="p" size="l">Test</Typography>
-    <Typography tagName="p" size="xl">Test</Typography>
-
-    <Typography tagName="p" bold size="xs">Test</Typography>
-    <Typography tagName="p" bold size="s">Test</Typography>
-    <Typography tagName="p" bold size="m">Test</Typography>
-    <Typography tagName="p" bold size="l">Test</Typography>
-    <Typography tagName="p" bold size="xl">Test</Typography>
-
-    <Typography tagName="h1" bold>Test</Typography>
-    <Typography tagName="h2" bold>Test</Typography>
-    <Typography tagName="h3" bold>Test</Typography>
-    <Typography tagName="h4" bold>Test</Typography>
-    <Typography tagName="h5" bold>Test</Typography>
+    <Header />
   </main>
 </template>
---
+++ src/shared/button/index.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import Typography from '@/shared/typography/ui.vue'
+interface Props {
+  color?: 'primary' | 'secondary' | 'grayscale' | 'error'
+  decoration?: 'default' | 'outline' | 'none'
+  size?: 'L' | 'M' | 'S'
+  disabled?: boolean
+}
+
+const props = defineProps<Props>()
+
+const { color = 'primary', decoration = 'default', size = 'M', disabled = false } = props
+const classes = ['button', `color_${color}`, `decoration_${decoration}`, `size_${size}`]
+</script>
+
+<template>
+  <button :class="classes" :disabled="disabled">
+    <slot name="leftIcon" />
+    <Typography class="button__text" tag-name="p" size="s"><slot /></Typography>
+    <slot name="rightIcon" />
+  </button>
+</template>
+
+<style scoped>
+.button {
+  display: flex;
+  align-items: center;
+  column-gap: 8px;
+  border-radius: 4px;
+  border: 1px solid transparent;
+  cursor: pointer;
+  user-select: none;
+  width: 100%;
+}
+.button.size_M {
+  padding: 8px;
+}
+
+.button.color_secondary {
+  background-color: var(--main-secondary);
+  border-color: var(--main-secondary);
+  color: var(--main-on-secondary);
+}
+
+.button__text {
+  width: 100%;
+  text-align: center;
+}
+</style>
