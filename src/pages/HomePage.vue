8c016c0ecc2bab3d1042a851b3983dd4bf4f14a7
matrosov <matrosov@100up.ru>
2024-04-01 00:22:28 +0400

    - typography

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Typography from '@/shared/typography/ui.vue'
+</script>
+
+<template>
+  <main>
+    <Typography tagName="p" size="s">Test</Typography>
+    <Typography tagName="p" size="xs">Test</Typography>
+    <Typography tagName="p" size="s">Test</Typography>
+    <Typography tagName="p" size="m">Test</Typography>
+    <Typography tagName="p" size="l">Test</Typography>
+    <Typography tagName="p" size="xl">Test</Typography>
+
+    <Typography tagName="p" bold size="xs">Test</Typography>
+    <Typography tagName="p" bold size="s">Test</Typography>
+    <Typography tagName="p" bold size="m">Test</Typography>
+    <Typography tagName="p" bold size="l">Test</Typography>
+    <Typography tagName="p" bold size="xl">Test</Typography>
+
+    <Typography tagName="h1" bold>Test</Typography>
+    <Typography tagName="h2" bold>Test</Typography>
+    <Typography tagName="h3" bold>Test</Typography>
+    <Typography tagName="h4" bold>Test</Typography>
+    <Typography tagName="h5" bold>Test</Typography>
+  </main>
+</template>
